35b9591eed8115619c0cd9a838d4aa9b39108056
Arlen Abraham <arlen.abraham@gmail.com>
2015-10-30 10:36:25 -0700

    fixed a double negative

--- src/scripts/youtube-pause-extension.js
+++ src/scripts/youtube-pause-extension.js
@@ -8,7 +8,7 @@
   a.onkeydown = responder;
   function responder(event) {
       // spacebar and not inside a the search or comments boxes
-      if (event.which == 32 && !((event.target.className.indexOf("search-term") !== -1) || (event.target.className.indexOf("yt-simplebox-text") !== -1))) {
+      if (event.which == 32 && ((event.target.className.indexOf("search-term") == -1) || (event.target.className.indexOf("yt-simplebox-text") == -1))) {
       var states = { 1: "playing", 2: "paused" };
       var p = window.document.getElementById("movie_player");
       var s = p && p.getPlayerState && states[p.getPlayerState()];
